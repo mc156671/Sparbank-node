acca15deb4824088ac6aec3a0d8d55ddb967df0a
Clemens <156646857+mc156671@users.noreply.github.com>
2024-09-23 05:49:08 +0000

    ..

--- uebungen/02-boolean.js
+++ uebungen/02-boolean.js
@@ -13,12 +13,25 @@ if(true){
     console.log("der Ausdruck ist unwahr / falsch / false")
 }
 
+//weil in dieser aufgabe die Prüfung immer war ist. wird immer die anweisung hinter if ausgeführt.
+//die anweisung hinterr else kommt niemals zur ausführung
+// ...
+
 console.log("Aufgabe 02")
 
 //In Erweiterung von Aufgabe 01 soll der Ausdruck dynamisch whar oder falsch sein.
 
 let a = 5
 
+//
+
+//Prüfoperatoren
+// == :prüfung auf glecihheit im wert
+// >    größer als
+// <    kleiner als
+// >=   größer oder gleich
+// <=   kleiner oder gleich
+// ===  prüfung auf gleichheit im wert und im typ
 if(a>6){
     console.log("true")
 } else {
@@ -45,3 +58,55 @@ if(alter <= 16 || fuehrerschein === false ){ //wenn dei schülerin 16 ist oder k
         }
 }
 
+
+console.log("aufgabe 4")
+//zahlen könne in javascript ture oder false sein.
+
+let zahl1 = 0;
+let zahl2 = 5;
+let zahl3 = NaN;
+
+if (zahl1) {
+    console.log("zahl1 ist true");
+
+} else {
+    console.log("zahl1 ist false");
+}
+
+if (zahl2) {
+    console.log("zahl2 ist true");
+} else {
+    console.log("zahl2 ist false");
+}
+
+if (zahl3) {
+    console.log("zahl3 ist true");
+} else {
+    console.log("zahl3 ist false");
+}
+
+// Null und NaN sind false alles ander ist true
+
+console.log("Aufgabe5")
+// auf einem sparkonto soll bei einem positiven kotostand auf der console stehen : zinsen berechen"
+// Wenn kein guthaben vorhanden : keien zinsen
+
+let kotostand = 1000
+
+if(kotostand){
+    console.log("zinsen berechnen")
+}else{
+    console.log("keien zinsen")
+}
+
+console.log("aufgabe 6")
+
+//
+
+alter = 1;
+
+if (alter){
+    console.log("alt");
+} else {
+    console.log("neu");
+}
